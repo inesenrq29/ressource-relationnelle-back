d3f17352a9501f0df6ba7f3f2a776e51e26a47ba
Inès <ines.enrique@icloud.com>
2026-03-11 15:14:28 +0100

    feat/repository: fix ResourceRepository and handle status update ResourceStatus by adding "RESTRICTED"

--- src/main/java/com/ienrique/ressourceRelationnelle/repository/ResourceRepository.java
+++ src/main/java/com/ienrique/ressourceRelationnelle/repository/ResourceRepository.java
@@ -1,19 +1,24 @@
 package com.ienrique.ressourceRelationnelle.repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.ienrique.ressourceRelationnelle.entity.Resource;
+import com.ienrique.ressourceRelationnelle.entity.ResourceStatus;
 
 @Repository
 public interface ResourceRepository extends JpaRepository<Resource, UUID> {
 
-  Resource findByResourceTitle(String resourceTitle);
+  Optional<Resource> findByResourceTitle(String resourceTitle);
 
   List<Resource> findByCategoryCategoryId(UUID categoryId);
 
   List<Resource> findByTagsTagId(UUID tagId);
+
+  List<Resource> findByStatus(
+      ResourceStatus status); // avec en status par exemple RESTRICTED pour ressource restreinte
 }
