0e3edd2d168abc58ac0a33a8579bee94a7927998
Inès <ines.enrique@icloud.com>
2026-03-26 14:50:08 +0100

    feat: implement unit test for service

--- src/main/java/com/ienrique/ressourceRelationnelle/dto/CreateResourceDto.java
+++ src/main/java/com/ienrique/ressourceRelationnelle/dto/CreateResourceDto.java
@@ -10,11 +10,13 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class CreateResourceDto {
 
   @NotBlank
