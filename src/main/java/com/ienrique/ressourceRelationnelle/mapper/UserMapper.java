40f88805a38a7b7986f78e17a92b0a0898159467
Sarah Testelin <sarah.khelil0709@gmail.com>
2026-03-27 12:57:02 +0100

    fix: springdoc upgrade + fix Friend entity mapping & UserMapper

--- src/main/java/com/ienrique/ressourceRelationnelle/mapper/UserMapper.java
+++ src/main/java/com/ienrique/ressourceRelationnelle/mapper/UserMapper.java
@@ -2,11 +2,15 @@ package com.ienrique.ressourceRelationnelle.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 
 import com.ienrique.ressourceRelationnelle.dto.UserDto;
 import com.ienrique.ressourceRelationnelle.entity.AppUser;
 
-@Mapper(componentModel = "spring", uses = RoleMapper.class)
+@Mapper(
+    componentModel = "spring",
+    uses = RoleMapper.class,
+    unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserMapper {
   @Mapping(target = "appUserId", source = "appUserId")
   UserDto toDto(AppUser user);
